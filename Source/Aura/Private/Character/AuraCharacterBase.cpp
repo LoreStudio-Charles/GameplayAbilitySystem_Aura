c0751395c73aebc9fc4d57e5196d1705a5ed47ac
Charles McCown <charles@lore.studio>
2023-10-03 21:26:31 -0500

    Create Character and Enemy classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright Charles A. McCown
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
